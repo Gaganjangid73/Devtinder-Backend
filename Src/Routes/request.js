2bbb77f0981f3a51e03cf1f69a5060fd0f0d408f
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-08 16:13:37 +0530

    pre. method or index in mongoose

--- Src/Routes/request.js
+++ Src/Routes/request.js
@@ -1,18 +1,65 @@
 const express = require("express");
 const requestrouter = express.Router();
 const userAuth = require("../middleware/userauth");
-
+const ConnectionRequest = require("../models/connectionrequest");
+const User = require("../models/user");
 
 // senduserrequest API
-requestrouter.post("/senduserrequest",userAuth, async (req,res)=>{
-   try {
-     const users = req.user;
-     res.send(users.firstName + " is send request successfully");
-   } catch (err) {
-      res.status(400).send("Something went wrong");
-   }
+requestrouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromuserId = req.user._id;
+      const touserId = req.params.toUserId;
+      const status = req.params.status;
 
-});
+      const ALLOWED = ["ignore", "interested"];
+      if (!ALLOWED.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+        
+      const user = await User.findById(touserId);
+      if(!user){
+         return res.status(404).json({message: "User  Not Found" });
+      }
 
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          {
+            fromuserId,
+            touserId,
+          },
+          {
+            fromuserId: touserId,
+            touserId: fromuserId,
+          },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection Request Already Exists !!" });
+      }
+      const connectionRequest = new ConnectionRequest({
+        fromuserId,
+        touserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection Request sent Succesfull ",
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Something went wrong i think something is not good ");
+    }
+  }
+);
 
 module.exports = requestrouter;
---
+++ Src/models/connectionrequest.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSChema = new mongoose.Schema(
+    {
+   fromuserId:{
+    type: mongoose.Schema.Types.ObjectId,
+    required:true,
+   },
+   touserId:{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+   },
+   status:{
+    type: String,
+    required :true,
+    enum: {
+        values : ["ignore", "interested", "accepeted" ,"rejected"],
+        message : `{VALUE} is the incorrect statua type`,
+    },
+   },
+    },{
+        timestamps:true,
+    }
+);
+
+connectionRequestSChema.pre("save", function(next){
+    const connectionRequest = this;
+    if(connectionRequest.fromuserId.equals(connectionRequest.touserId)){
+     throw new Error("Cannot send conection request to yourself");
+    }
+    next();
+});
+
+const connectionRequest = new mongoose.model("connectionRequest", connectionRequestSChema);
+
+module.exports = connectionRequest;
